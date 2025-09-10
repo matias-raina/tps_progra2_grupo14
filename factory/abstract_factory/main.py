98ac9c554ed6ce4cf4c535ef72970ad4c729f293
Marcos Achaval <marcos.achavalr@gmail.com>
2025-09-10 18:07:56 -0300

    Resolucion paso 2

--- factory/abstract_factory/main.py
+++ factory/abstract_factory/main.py
@@ -1,9 +1,14 @@
 from .store import NYPizzaStore, ChicagoPizzaStore
 
+
 def main():
-    ny = NYPizzaStore(); chi = ChicagoPizzaStore()
+    ny = NYPizzaStore()
+    chi = ChicagoPizzaStore()
     ny.order_pizza("cheese")
     chi.order_pizza("clam")
+    ny.order_pizza("veggie")
+    chi.order_pizza("pepperoni")
+
 
 if __name__ == "__main__":
     main()
--- factory/abstract_factory/pizza.py
+++ factory/abstract_factory/pizza.py
@@ -1,25 +1,63 @@
 from abc import ABC, abstractmethod
 from .ingredients import PizzaIngredientFactory
 
+
 class Pizza(ABC):
     def __init__(self, name: str, ing_factory: PizzaIngredientFactory):
-        self.name=name; self.f=ing_factory
-        self.dough=None; self.sauce=None; self.cheese=None; self.clam=None
+        self.name = name
+        self.f = ing_factory
+        self.dough = None
+        self.sauce = None
+        self.cheese = None
+        self.clam = None
+        self.veggies = None
+        self.pepperoni = None
+
     @abstractmethod
     def prepare(self): ...
     def bake(self): print("Bake 25 min at 350")
-    def cut(self):  print("Cutting pizza into diagonal slices")
-    def box(self):  print("Place pizza in official box")
+    def cut(self): print("Cutting pizza into diagonal slices")
+    def box(self): print("Place pizza in official box")
     def __str__(self): return self.name
+
 
 class CheesePizza(Pizza):
     def prepare(self):
         print(f"Preparing {self.name}")
-        self.dough=self.f.create_dough(); self.sauce=self.f.create_sauce(); self.cheese=self.f.create_cheese()
+        self.dough = self.f.create_dough()
+        self.sauce = self.f.create_sauce()
+        self.cheese = self.f.create_cheese()
         print(" ->", self.dough, "/", self.sauce, "/", self.cheese)
+
 
 class ClamPizza(Pizza):
     def prepare(self):
         print(f"Preparing {self.name}")
-        self.dough=self.f.create_dough(); self.sauce=self.f.create_sauce(); self.cheese=self.f.create_cheese(); self.clam=self.f.create_clam()
-        print(" ->", self.dough, "/", self.sauce, "/", self.cheese, "/", self.clam)
+        self.dough = self.f.create_dough()
+        self.sauce = self.f.create_sauce()
+        self.cheese = self.f.create_cheese()
+        self.clam = self.f.create_clam()
+        print(" ->", self.dough, "/", self.sauce,
+              "/", self.cheese, "/", self.clam)
+
+
+class VeggiePizza(Pizza):
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        self.dough = self.f.create_dough()
+        self.sauce = self.f.create_sauce()
+        self.cheese = self.f.create_cheese()
+        self.veggies = self.f.create_veggies()
+        print(" ->", self.dough, "/", self.sauce,
+              "/", self.cheese, "/", self.veggies)
+
+
+class PepperoniPizza(Pizza):
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        self.dough = self.f.create_dough()
+        self.sauce = self.f.create_sauce()
+        self.cheese = self.f.create_cheese()
+        self.pepperoni = self.f.create_pepperoni()
+        print(" ->", self.dough, "/", self.sauce,
+              "/", self.cheese, "/", self.pepperoni)
